1f3fc7d00aa7618e210d78128656ff543474d0fa
Soham <s17deshpande@gmail.com>
2025-07-12 04:25:41 +0530

    Insert Query

--- src/main/java/com/example/demo/controller/IOMRequestController.java
+++ src/main/java/com/example/demo/controller/IOMRequestController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.dto.ApiResponse;
 import com.example.demo.dto.IOMRequestDto;
 import com.example.demo.entity.IOMRequestMaster;
+import com.example.demo.repository.IomRequestRepository;
 import com.example.demo.service.IOMRequestService;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,27 +13,46 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Map;
+import java.util.OptionalInt;
 
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("/api/")
 public class IOMRequestController {
 
+//    @Autowired
+//    private IOMRequestService iomRequestService;
+//
+//    @PostMapping("iom-create")
+//    public ResponseEntity<?> createIomRequest(@RequestBody IOMRequestDto iomRequestDto){
+//
+//        IOMRequestMaster iomRequest = iomRequestService.createIOMRequest(iomRequestDto);
+//
+//        ApiResponse<Object> iomReuest = ApiResponse.builder()
+//                .status(HttpStatus.OK.value())
+//                .message("IOM request created successfully")
+//                .data(Collections.emptyList())
+//                .build();
+//
+//        return ResponseEntity.ok(iomReuest);
+
+
     @Autowired
-    private IOMRequestService iomRequestService;
+    IomRequestRepository iomRequestRepository;
 
-    @PostMapping("iom-create")
-    public ResponseEntity<?> createIomRequest(@RequestBody IOMRequestDto iomRequestDto){
 
-        IOMRequestMaster iomRequest = iomRequestService.createIOMRequest(iomRequestDto);
+    @PostMapping("createNewRequest")
+    public ResponseEntity<?> createIomRequest(@RequestBody Map<String,String> iomRequestDto) {
 
-        ApiResponse<Object> iomReuest = ApiResponse.builder()
-                .status(HttpStatus.OK.value())
-                .message("IOM request created successfully")
-                .data(Collections.emptyList())
-                .build();
+        OptionalInt res = iomRequestRepository.insertRequest(iomRequestDto);
 
-        return ResponseEntity.ok(iomReuest);
+        if(res.isEmpty()){
+            return ResponseEntity.badRequest().build();
+        }
+
+        return ResponseEntity.ok(res.getAsInt());
+
     }
 
 }
--- src/main/java/com/example/demo/repository/IomRequestRepository.java
+++ src/main/java/com/example/demo/repository/IomRequestRepository.java
@@ -2,10 +2,16 @@ package com.example.demo.repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.OptionalInt;
 
 @Repository
 public class IomRequestRepository {
@@ -21,6 +27,33 @@ public class IomRequestRepository {
         String query = "SELECT * FROM requests where request_id = "+requestId;
         var data = jdbcTemplate.queryForList(query);
         return data;
+    }
+
+//    public OptionalInt insertRequest(String title, String category, String subCategory, String description) {
+    public OptionalInt insertRequest(Map<String,String> data) {
+        String sql = "INSERT INTO requests (title, category, sub_category, description) VALUES (?, ?, ?, ?)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        if((Objects.isNull(data.get("title")) && data.get("title").isEmpty()) || (Objects.isNull(data.get("category")) && data.get("category").isEmpty()) ||
+                (Objects.isNull(data.get("subCategory")) && data.get("subCategory").isEmpty()) || (Objects.nonNull(data.get("description")) && data.get("description").isEmpty()) ){
+            return OptionalInt.empty();
+        }
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, data.get("title"));
+            ps.setString(2, data.get("category"));
+            ps.setString(3, data.get("subCategory"));
+            ps.setString(4, data.get("description"));
+            return ps;
+        }, keyHolder);
+
+        Map<String, Object> keys = keyHolder.getKeys();
+        if (keys != null && keys.containsKey("request_id")) {
+            return OptionalInt.of(((Number) keys.get("request_id")).intValue());
+        } else {
+            return OptionalInt.empty();
+        }
     }
 
 }
