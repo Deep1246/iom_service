ad2da36c68880b727a5b20da982a6fec179a3ce3
Soham <s17deshpande@gmail.com>
2025-07-12 06:05:12 +0530

    updated insert apis

--- src/main/java/com/example/demo/controller/IOMRequestController.java
+++ src/main/java/com/example/demo/controller/IOMRequestController.java
@@ -1,48 +1,46 @@
-package com.example.demo.controller;
-
-import com.example.demo.dto.ApiResponse;
-import com.example.demo.dto.IOMRequestDto;
-import com.example.demo.dto.WorkflowActionDto;
-import com.example.demo.entity.IOMRequestMaster;
-import com.example.demo.repository.IomRequestRepository;
-import com.example.demo.service.IOMRequestService;
-import org.apache.coyote.Response;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Map;
-import java.util.OptionalInt;
-
-@RestController
-@CrossOrigin(origins = "*")
-@RequestMapping("/api/")
-public class IOMRequestController {
-
-    @Autowired
-    IomRequestRepository iomRequestRepository;
-
-
-    @PostMapping("createNewRequest")
-    public ResponseEntity<?> createIomRequest(@RequestBody Map<String,String> iomRequestDto) {
-
-        OptionalInt res = iomRequestRepository.insertRequest(iomRequestDto);
-
-        if(res.isEmpty()){
-            return ResponseEntity.badRequest().build();
-        }
-
-        return ResponseEntity.ok(res.getAsInt());
-
-    }
-
-//    @PostMapping("workflow-action")
-//    public ResponseEntity<?> approveWorkflow(@RequestBody WorkflowActionDto workflowActionDto){
+//package com.example.demo.controller;
 //
-//        return ResponseEntity.ok();
+//import com.example.demo.dto.ApiResponse;
+//import com.example.demo.dto.IOMRequestDto;
+//import com.example.demo.dto.WorkflowActionDto;
+//import com.example.demo.entity.IOMRequestMaster;
+//import com.example.demo.repository.IomRequestRepository;
+//import com.example.demo.service.IOMRequestService;
+//import org.apache.coyote.Response;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.*;
+//
+//@RestController
+//@CrossOrigin(origins = "*")
+//@RequestMapping("/api/")
+//public class IOMRequestController {
+//
+//    @Autowired
+//    IomRequestRepository iomRequestRepository;
+//
+//
+//    @PostMapping("createNewRequest")
+//    public ResponseEntity<?> createIomRequest(@RequestBody Map<String,String> iomRequestDto) {
+//
+//        OptionalInt res = iomRequestRepository.insertRequest(iomRequestDto);
+//
+//        if(res.isEmpty()){
+//            return ResponseEntity.badRequest().build();
+//        }
+//        Map<String, Object> response = new HashMap<>();
+//        response.put("request_id", res.getAsInt());
+//        return ResponseEntity.ok().body(response);
+//
 //    }
-
-}
+//
+////    @PostMapping("workflow-action")
+////    public ResponseEntity<?> approveWorkflow(@RequestBody WorkflowActionDto workflowActionDto){
+////
+////        return ResponseEntity.ok();
+////    }
+//
+//}
